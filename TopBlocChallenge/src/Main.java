e0e7d26cc6ba073979fa5169e2402a553543f126
ENBT <ian@enbt.net>
2020-08-31 13:36:19 -0500

    get response and add built .jar

--- TopBlocChallenge/src/Main.java
+++ TopBlocChallenge/src/Main.java
@@ -1,8 +1,12 @@
 import java.sql.Connection;
+
+import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.HttpEntity;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 
 public class Main 
@@ -33,6 +37,7 @@ public class Main
 	{
 		HttpClient httpClient = HttpClientBuilder.create().build();
 		HttpPost request = new HttpPost("http://54.90.99.192:5000/challenge");
+		JSONObject result = new JSONObject();
 		try
 		{			
             String bodyContent = new JSONObject()
@@ -46,8 +51,13 @@ public class Main
             
             request.setEntity(requestBody);
             request.setHeader("Content-Type", "application/json");
-            httpClient.execute(request);
-            }
+            HttpResponse response = httpClient.execute(request);
+            
+            HttpEntity entity = response.getEntity();
+            String responseString = EntityUtils.toString(entity);
+            result.put("status", response.getStatusLine().getStatusCode());
+            result.put("bodyContent", new JSONObject(responseString));
+        }
 		catch (Exception ex)
 		{
 			System.out.println(ex.getMessage());
